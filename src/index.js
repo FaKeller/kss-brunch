8e49bac2c69ffa70b5e87538451c3c8070ff7fa0
Fabian Keller <hello@fabian-keller.de>
2017-02-12 18:24:23 +0100

    Feature: basic implementation of kss node plugin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,58 @@
 'use strict';
 
+let kss = require('kss'),
+    _ = require('lodash'),
+    fs = require('fs');
+
+const DEFAULT_OPTIONS = {
+    /** include generated CSS files in the KSS styleguide */
+    addCssFiles: true,
+    /** include generated CSS files in the KSS styleguide */
+    addJsFiles: true,
+
+    /** options that are passed to kss-node */
+    kssConfig: {}
+};
+
 class KssPlugin {
 
     constructor(config) {
-        // Replace 'plugin' with your plugin's name;
-        this.config = config.plugins.kss || {
-
-            };
+        this.brunchConfig = config;
+        this.kssConfig = _.defaultsDeep(
+            config.plugins.kss || {},
+            {
+                kssConfig: {
+                    source: _.filter(this.brunchConfig.paths.watched, path => fs.existsSync(path)),
+                    destination: `${this.brunchConfig.paths.public}/styleguide`
+                }
+            },
+            DEFAULT_OPTIONS
+        );
     }
 
     /**
      * Brunch Hook after everything has compiled.
      */
     onCompile(files) {
-        console.log("Generating living styleguide", files);
+        console.log("Generating living styleguide");
+        let actualConfig = _.cloneDeep(this.kssConfig.kssConfig);
+
+        if (this.kssConfig.addCssFiles) {
+            let cssFiles = _(files)
+                .filter(file => file.type == 'stylesheet')
+                .map(sf => sf.path)
+                .value();
+            actualConfig.css = (actualConfig.css || []).concat(cssFiles);
+        }
+        if (this.kssConfig.addJsFiles) {
+            let jsFiles = _(files)
+                .filter(file => file.type == 'javascript')
+                .map(sf => sf.path)
+                .value();
+            actualConfig.js = (actualConfig.js || []).concat(jsFiles);
+        }
+
+        return kss(actualConfig);
     }
 }
 
